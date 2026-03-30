2e63d511f3e092b169e3ed56613b5fad2d955b4d
shenesuelo <>
2026-03-30 12:01:48 +0800

    fic the button

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let currentQuote = "";
 let currentAuthor = "";
 
-// Get quote from API
 async function getQuote() {
     try {
-        const response = await fetch("https://api.quotable.io/random");
+        const response = await fetch("https://dummyjson.com/quotes/random");
         const data = await response.json();
 
-        currentQuote = data.content;
+        currentQuote = data.quote;
         currentAuthor = data.author;
 
         document.getElementById("quote").innerText = currentQuote;
@@ -15,36 +14,6 @@ async function getQuote() {
 
     } catch (error) {
         document.getElementById("quote").innerText = "Failed to load quote.";
+        console.error("Error:", error);
     }
-}
-
-// Save quote to localStorage
-function saveQuote() {
-    if (!currentQuote) return;
-
-    let favorites = JSON.parse(localStorage.getItem("quotes")) || [];
-
-    favorites.push({
-        quote: currentQuote,
-        author: currentAuthor
-    });
-
-    localStorage.setItem("quotes", JSON.stringify(favorites));
-
-    alert("Quote saved!");
-}
-
-// Display saved quotes
-function viewFavorites() {
-    let favorites = JSON.parse(localStorage.getItem("quotes")) || [];
-    let container = document.getElementById("favorites");
-
-    container.innerHTML = "<h2>Saved Quotes:</h2>";
-
-    favorites.forEach(q => {
-        container.innerHTML += `
-            <p>"${q.quote}"<br><small>- ${q.author}</small></p>
-            <hr>
-        `;
-    });
 }
